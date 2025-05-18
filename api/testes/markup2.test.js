fb6432b95496e284da3269f3efac915db707181d
Tiago Silva <tiago.ps2@puccampinas.edu.br>
2025-05-18 19:38:53 -0300

    test: adiciona testes para as rotas da API

--- api/testes/markup2.test.js
+++ api/testes/markup2.test.js
@@ -32,3 +32,35 @@ test('Teste de divisão de markup com valores zero', () => {
 test('Teste de divisão de markup com valores decimais', () => {
     expect(markup2.calcularMarkupDivisor(100.5, 50.25)).toBe(0.5);
 });
+
+// NOVOS TESTES USANDO TDD - Tiago
+// Teste para a rota POST /MKP2/calcMultiplierMarkup
+test("POST to /MKP2/calcMultiplierMarkup should return 201", async () => {
+    const response = await fetch('http://localhost:3000/MKP2/calcMultiplierMarkup', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            despesasVariaveis: 10,
+            despesasFixas: 20,
+            margemLucro: 30
+        })
+    });
+
+    /* Aqui podemos testar se o calculo do markup está correto
+    const responsebody = await response.text();
+    console.log(responsebody);
+    expect(responsebody).toBe("2.5");
+    */
+
+    expect(response.status).toBe(200);
+});
+
+test('Teste de multiplicação de markup com valores não numéricos', () => {
+    expect(markup2.calcularMarkup('a', 'b', 'c')).toBe("Erro");
+});
+
+test('Teste de multiplicação de markup com valores numeros e não numéricos', () => {
+    expect(markup2.calcularMarkup(10, 'b', 30)).toBe("Erro");
+});
