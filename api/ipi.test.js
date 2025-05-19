41db199a77522837b87614106224e40a8444f6f6
guildpuc <guilherme.ld1@puccampinas.edu.br>
2025-05-19 18:59:11 -0300

    criação da api "ipi" e alterações para rodar os testes no github actions

---
+++ api/ipi.test.js
@@ -0,0 +1,22 @@
+const request = require('supertest');
+const app = require('./server'); // importa o app configurado
+
+describe('Testa rota de cálculo de IPI', () => {
+  it('deve calcular corretamente o IPI', async () => {
+    const res = await request(app)
+      .post('/ipi')
+      .send({ valorProduto: 100, aliquotaIPI: 10 });
+
+    expect(res.statusCode).toBe(200);
+    expect(res.body.imposto).toBe("10.00");
+  });
+
+  it('deve retornar erro se faltar campos', async () => {
+    const res = await request(app)
+      .post('/ipi')
+      .send({ valorProduto: 100 });
+
+    expect(res.statusCode).toBe(400);
+    expect(res.body.erro).toBe('Informe valorProduto e aliquotaIPI');
+  });
+});
--- api/server.js
+++ api/server.js
@@ -3,26 +3,25 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 const cors = require('cors');
 const ipiRoutes = require('./routes/ipiRoutes');
-
-
+const userRoutes = require('./routes/userRoutes');
+const baseRoutes = require('./routes/baseRoutes');
 
 const app = express();
 const port = 3000;
 
-// importa as rotas
-const userRoutes = require('./routes/userRoutes')
-const baseRoutes = require('./routes/baseRoutes')
-
-app.use(cors()); // Enable CORS for all routes
+app.use(cors());
 app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/', baseRoutes);
 app.use('/users', userRoutes);
 app.use('/impostos', ipiRoutes);
 
+// Só escuta a porta se não estiver em ambiente de testes
+if (process.env.NODE_ENV !== 'test') {
+  app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+    console.log(`Swagger em http://localhost:${port}/api-docs`);
+  });
+}
 
-
-app.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}`);
-  console.log(`Swagger em http://localhost:${port}/api-docs`);
-});
+module.exports = app;
