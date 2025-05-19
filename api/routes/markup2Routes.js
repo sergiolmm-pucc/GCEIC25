a81352d92a0a96235a41637e0550e53ddb3b1344
Enzo Quatrochi <enzoquatrochi@outlook.com>
2025-05-19 20:02:22 -0300

    desenvolvimento da tela e api sobre

--- api/routes/markup2Routes.js
+++ api/routes/markup2Routes.js
@@ -5,4 +5,5 @@ const markup2Controller = require('../controllers/markup2Controller');
 
 router.post('/calcMultiplierMarkup', markup2Controller.calcMultiplierMarkup);
 router.post('/calcDivisorMarkup', markup2Controller.calcDivisorMarkup);
+router.get('/sobre', markup2Controller.retornarSobre);
 module.exports = router;
