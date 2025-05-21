db8c5b34247ad2b4b68689e399679b507b9aa00f
marcelafranc <marcelasfranco1@gmail.com>
2025-05-21 19:54:39 -0300

    Markup controller e route

--- api/server.js
+++ api/server.js
@@ -10,13 +10,15 @@ const port = 3000;
 // importa as rotas
 const userRoutes = require('./routes/userRoutes')
 const baseRoutes = require('./routes/baseRoutes')
+const markupRoutes = require('./routes/markupRoutes');
+
 
 app.use(cors()); // Enable CORS for all routes
 app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/', baseRoutes);
 app.use('/users', userRoutes);
-
+app.use('/', markupRoutes);
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
