b1088a58e68bb03be0fbbf6e0c295b0b55a380a3
Bruno Savoia <brunotsavoia@gmail.com>
2025-05-21 19:18:08 -0300

    pagina home

---
+++ web/gcseic25/lib/equipes/CI_CD_10/calcular.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CalcularPage extends StatelessWidget {
+  const CalcularPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- web/gcseic25/lib/equipes/CI_CD_10/home.dart
+++ web/gcseic25/lib/equipes/CI_CD_10/home.dart
@@ -1,10 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:gcseic25/equipes/CI_CD_10/calcular.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Grupo CI/CD 8'), centerTitle: true),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Text(
+                'Cálculo de Encargos trabalhistas de Empregada doméstica',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+              ),
+              const SizedBox(height: 30),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (_) => const CalcularPage()),
+                  );
+                },
+                child: const Text('Ir para cálculo'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
