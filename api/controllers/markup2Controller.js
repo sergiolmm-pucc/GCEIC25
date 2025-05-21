98e1ea7fdafdf8937728616fe687bdb4ed1a7430
VinMendes <vinicius.mc6@puccampinas.edu.br>
2025-05-21 11:20:56 -0300

    Teste authentication prontos

--- api/controllers/markup2Controller.js
+++ api/controllers/markup2Controller.js
@@ -20,18 +20,30 @@ function auth (req, res) {
   const { email, senha } = req.body;
 
   // Credenciais fixas para autenticação
-  const EMAIL_CORRETO = "admin@exemplo.com";
-  const SENHA_CORRETA = "123456";
 
-  if (email === EMAIL_CORRETO && senha === SENHA_CORRETA) {
+  const isAuth = autentication(email, senha)
+
+  if (isAuth) {
     return res.json({ acesso: 'liberado' });
   } else {
     return res.status(401).json({ acesso: 'negado' });
   }
 };
 
+function autentication(email, password) {
+    const EMAIL_CORRETO = "admin@exemplo.com";
+    const SENHA_CORRETA = "123456";
+
+    if (email === EMAIL_CORRETO && password === SENHA_CORRETA) {
+      return true
+    } else {
+      return false
+    }
+}
+
 module.exports = {
     calcMultiplierMarkup,
     calcularMarkup,
-    auth
+    auth,
+    autentication
 };
