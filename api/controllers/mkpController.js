70057af7f647ce68828d589d8d3b47237b065048
filipe mota <filipe.dmtm@puccampinas.edu.br>
2025-05-22 08:27:36 -0300

    Criando controlador e rota de cáculo do lucro obtido a partir do preço de venda

--- api/controllers/mkpController.js
+++ api/controllers/mkpController.js
@@ -5,3 +5,11 @@ exports.calculoSimples = (req, res) => {
     const precoVenda = custo * (1 + lucro);
     res.json({ precoVenda: precoVenda.toFixed(2) });
   };
+
+// Cálculo do lucro obtido
+exports.lucroObtido = (req, res) => {
+    const { custo, precoVenda } = req.body;
+    if (!custo || !precoVenda) return res.status(400).json({ error: 'Campos obrigatórios: custo e precoVenda' });
+    const lucro = (precoVenda - custo) / custo;
+    res.json({ lucro: (lucro * 100).toFixed(2) + '%' });
+};
--- api/routes/mkpRoutes.js
+++ api/routes/mkpRoutes.js
@@ -5,5 +5,7 @@ const mkpController = require('../controllers/mkpController');
 
 router.post('/markup-simples', mkpController.calculoSimples);
 
+//Rota do lucro obtido
+router.post('/lucro-obtido', markupController.lucroObtido);
 
 module.exports = router;
