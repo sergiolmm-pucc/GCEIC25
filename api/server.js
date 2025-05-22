ad03defb9af66826b6845d9716120eee8641882d
Bruno Savoia <brunotsavoia@gmail.com>
2025-05-22 08:37:10 -0300

    correcao

--- api/server.js
+++ api/server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/', baseRoutes);
 app.use('/users', userRoutes);
-app.use('/etec/', etecRoutes);
+app.use('/etec', etecRoutes);
 
 
 app.listen(port, () => {
