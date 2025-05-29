c95ce9c7783b3b5b2cdc10c79122a46f3076daa6
melseleghin <melseleghin@gmail.com>
2025-05-29 09:14:12 -0300

    api and route

--- api/routes/mkp1Routes.js
+++ api/routes/mkp1Routes.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const markupController = require('../controllers/mkp1Controller');
 
 router.post('/markup-simples', markupController.calculoSimples);
-
+router.post('/sugestao-preco', markupController.sugestaoPreco);
 
 module.exports = router;
