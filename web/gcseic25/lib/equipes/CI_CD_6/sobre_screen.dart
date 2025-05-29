20badbe59b00d3376867326a3f7ad51d80bb40ee
guilherme-hl1ma <guihlima@outlook.com>
2025-05-29 10:05:19 -0300

    adicionar seção SOBRE + adicionar as fotos dos membros da equipe

--- web/gcseic25/lib/equipes/CI_CD_6/sobre_screen.dart
+++ web/gcseic25/lib/equipes/CI_CD_6/sobre_screen.dart
@@ -8,6 +8,29 @@ class SobreScreen extends StatefulWidget {
 }
 
 class _SobreScreenState extends State<SobreScreen> {
+  final List<Map<String, String>> membros = [
+    {
+      'nome': 'Arthur',
+      'foto': 'lib/equipes/CI_CD_6/assets/arthur.jpeg',
+    },
+    {
+      'nome': 'Guilherme',
+      'foto': 'lib/equipes/CI_CD_6/assets/guilherme.jpeg',
+    },
+    {
+      'nome': 'João',
+      'foto': 'lib/equipes/CI_CD_6/assets/joão.jpeg',
+    },
+    {
+      'nome': 'Vinicius',
+      'foto': 'lib/equipes/CI_CD_6/assets/vinicius.jpeg',
+    },
+    {
+      'nome': 'Felipe',
+      'foto': 'lib/equipes/CI_CD_6/assets/felipe.jpeg',
+    }
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,7 +41,28 @@ class _SobreScreenState extends State<SobreScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('Sobre'),
+            const Text(
+              'Equipe CI/CD 6',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 32),
+            ...membros.map((membro) => Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      CircleAvatar(
+                        radius: 40,
+                        backgroundImage: AssetImage(membro['foto']!),
+                      ),
+                      const SizedBox(width: 24),
+                      Text(
+                        membro['nome']!,
+                        style: const TextStyle(fontSize: 20),
+                      ),
+                    ],
+                  ),
+                )),
           ],
         ),
       ),
