d4dea8689d85c6dfa9013578d2dea65230d0245a
Guilherme <guilhermedallora100@gmail.com>
2025-05-29 20:19:33 -0300

    ajustes

--- api/eslint.config.mjs
+++ api/eslint.config.mjs
@@ -5,12 +5,8 @@ import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-<<<<<<< HEAD
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
+  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: {...globals.browser, ...globals.node, ...globals.commonjs} } },
 
 ]);
-=======
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: { ...globals.node, ...globals.commonjs } } },
-]);
->>>>>>> upstream/main
+
