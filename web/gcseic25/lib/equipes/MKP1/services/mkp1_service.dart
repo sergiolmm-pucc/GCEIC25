04d6826424790c26bbec0727479f98570046f574
João Pedro Giaretta <80277157+Joao-Giaretta@users.noreply.github.com>
2025-05-29 21:40:20 -0300

    Update mkp1_service.dart

--- web/gcseic25/lib/equipes/MKP1/services/mkp1_service.dart
+++ web/gcseic25/lib/equipes/MKP1/services/mkp1_service.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class MKP1Service {
-  static const String baseUrl = 'https://animated-occipital-buckthorn.glitch.me/mkp1/';
+  static const String baseUrl = 'https://animated-occipital-buckthorn.glitch.me/mkp1';
 
   // Cálculo simples de markup
   Future<Map<String, dynamic>> calculoSimples(
