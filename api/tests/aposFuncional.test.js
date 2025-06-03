eec909ebe050552603fe5d82ffea8c21c756eed9
Isabella <117479295+isabellatressino@users.noreply.github.com>
2025-06-02 21:16:49 -0300

    teste funcional calculo de aposentadoria

--- api/tests/aposFuncional.test.js
+++ api/tests/aposFuncional.test.js
@@ -1,11 +1,11 @@
 const { Builder } = require('selenium-webdriver');
 const { FlutterSeleniumBridge } = require('@rentready/flutter-selenium-bridge');
-const { Browser, By,Key,  until } = require('selenium-webdriver');
+const { Browser, By, Key, until } = require('selenium-webdriver');
 const { Options } = require('selenium-webdriver/chrome');
 
 (async () => {
-   // Configuração do ambiente do WebDriver e opções do navegador
-   const screen = {
+  // Configuração do ambiente do WebDriver e opções do navegador
+  const screen = {
     width: 1024,
     height: 720
   };
@@ -13,10 +13,9 @@ const { Options } = require('selenium-webdriver/chrome');
   console.log('Config chrome');
   const chromeOptions = new Options();
   // comentar as linhas abaixo quando for subir para teste local
-//  chromeOptions.addArguments('--headless');
-//  chromeOptions.addArguments('--no-sandbox');
-//  chromeOptions.windowSize(screen);
-
+  //  chromeOptions.addArguments('--headless');
+  //  chromeOptions.addArguments('--no-sandbox');
+  //  chromeOptions.windowSize(screen);
 
   console.log('ini builder');
   const builder = new Builder()
@@ -24,7 +23,6 @@ const { Options } = require('selenium-webdriver/chrome');
     .setChromeOptions(chromeOptions);
 
   // Criação da instância do WebDriver
-
   console.log('driver creation');
   let driver = await builder.build();
 
@@ -46,46 +44,45 @@ const { Options } = require('selenium-webdriver/chrome');
     });
   });
 
-await driver.wait(until.elementLocated(By.xpath("//flt-semantics[text()='Calculadora de Aposentadoria']")), 15000);
-const calcButton = await driver.findElement(By.xpath("//flt-semantics[text()='Calculadora de Aposentadoria']"));
-await calcButton.click();
-await driver.sleep(5000);
-await driver.takeScreenshot().then((image, err) => {
-  require('fs').writeFile('../fotos/APOS/splash_screen.png', image, 'base64', function (err) {
-    if (err == null) {
-      console.log('Gravou Foto da Splash da Aposentadoria');
-    } else {
-      console.log('Erro ->' + err);
-    }
-  });
-});
-
-await driver.sleep(10000);
-await driver.takeScreenshot().then((image, err) => {
-    require('fs').writeFile('../fotos/APOS/login_screen.png', image, 'base64', function (err) {
-      if (err == null){
-          console.log('Gravou Foto da Tela de Login');
-      }else{
-          console.log('Erro ->' + err);
+  await driver.wait(until.elementLocated(By.xpath("//flt-semantics[text()='Calculadora de Aposentadoria']")), 15000);
+  const calcButton = await driver.findElement(By.xpath("//flt-semantics[text()='Calculadora de Aposentadoria']"));
+  await calcButton.click();
+  await driver.sleep(5000);
+  await driver.takeScreenshot().then((image, err) => {
+    require('fs').writeFile('../fotos/APOS/splash_screen.png', image, 'base64', function (err) {
+      if (err == null) {
+        console.log('Gravou Foto da Splash da Aposentadoria');
+      } else {
+        console.log('Erro ->' + err);
       }
-
     });
   });
 
-await driver.wait(until.elementLocated(By.xpath("//*[@aria-label='Usuário']")), 15000);
-await driver.findElement(By.xpath("//*[@aria-label='Usuário']")).sendKeys('admin');
-await driver.wait(until.elementLocated(By.xpath("//*[@aria-label='Senha']")), 15000);
-await driver.findElement(By.xpath("//*[@aria-label='Senha']")).sendKeys('1234');
-await driver.wait(until.elementLocated(By.xpath("//flt-semantics[text()='Entrar']")), 15000);
-const loginButton = await driver.findElement(By.xpath("//flt-semantics[text()='Entrar']"));
-await loginButton.click();
-await driver.sleep(5000);
-await driver.takeScreenshot().then((image, err) => {
+  await driver.sleep(10000);
+  await driver.takeScreenshot().then((image, err) => {
+    require('fs').writeFile('../fotos/APOS/login_screen.png', image, 'base64', function (err) {
+      if (err == null) {
+        console.log('Gravou Foto da Tela de Login');
+      } else {
+        console.log('Erro ->' + err);
+      }
+    });
+  });
+
+  await driver.wait(until.elementLocated(By.xpath("//*[@aria-label='Usuário']")), 15000);
+  await driver.findElement(By.xpath("//*[@aria-label='Usuário']")).sendKeys('admin');
+  await driver.wait(until.elementLocated(By.xpath("//*[@aria-label='Senha']")), 15000);
+  await driver.findElement(By.xpath("//*[@aria-label='Senha']")).sendKeys('1234');
+  await driver.wait(until.elementLocated(By.xpath("//flt-semantics[text()='Entrar']")), 15000);
+  const loginButton = await driver.findElement(By.xpath("//flt-semantics[text()='Entrar']"));
+  await loginButton.click();
+  await driver.sleep(5000);
+  await driver.takeScreenshot().then((image, err) => {
     require('fs').writeFile('../fotos/APOS/home_screen.png', image, 'base64', function (err) {
-      if (err == null){
-          console.log('Gravou Foto da Tela Inicial');
-      }else{
-          console.log('Erro ->' + err);
+      if (err == null) {
+        console.log('Gravou Foto da Tela Inicial');
+      } else {
+        console.log('Erro ->' + err);
       }
     });
   });
@@ -97,43 +94,84 @@ await driver.takeScreenshot().then((image, err) => {
   await aboutButton.click();
   await driver.sleep(5000);
   await driver.takeScreenshot().then((image, err) => {
-      require('fs').writeFile('../fotos/APOS/about_screen.png', image, 'base64', function (err) {
-        if (err == null){
-            console.log('Gravou Foto da Tela Sobre');
-        }else{
-            console.log('Erro ->' + err);
+    require('fs').writeFile('../fotos/APOS/about_screen.png', image, 'base64', function (err) {
+      if (err == null) {
+        console.log('Gravou Foto da Tela Sobre');
+      } else {
+        console.log('Erro ->' + err);
+      }
+    });
+  });
+
+  const backButton = await driver.wait(
+    until.elementLocated(By.xpath("//flt-semantics[@role='button' and contains(text(), 'Back')]")),
+    15000
+  );
+  await backButton.click();
+
+  const helpButton = await driver.wait(
+    until.elementLocated(By.xpath("//flt-semantics[@role='button' and contains(text(), 'Ajuda')]")),
+    15000
+  );
+  await helpButton.click();
+  await driver.sleep(5000);
+  await driver.takeScreenshot().then((image, err) => {
+    require('fs').writeFile('../fotos/APOS/help_screen.png', image, 'base64', function (err) {
+      if (err == null) {
+        console.log('Gravou Foto da Tela de Ajuda');
+      } else {
+        console.log('Erro ->' + err);
+      }
+    });
+  });
+
+  const backButton2 = await driver.wait(
+    until.elementLocated(By.xpath("//flt-semantics[@role='button' and contains(text(), 'Back')]")),
+    15000
+  );
+  await backButton2.click();
+
+  const calcAposButton = await driver.wait(
+    until.elementLocated(By.xpath("//flt-semantics[@role='button' and contains(text(), 'Calcular aposentadoria')]")),
+    10000
+  );
+  await calcAposButton.click();
+
+  await driver.takeScreenshot().then((image, err) => {
+      require('fs').writeFile('../fotos/APOS/calculo_aposentadoria.png', image, 'base64', function (err) {
+        if (err == null) {
+          console.log('Gravou Foto da Tela de Calculo de Aposentadoria');
+        } else {
+          console.log('Erro ->' + err);
         }
       });
     });
 
-    const backButton = await driver.wait(
-      until.elementLocated(By.xpath("//flt-semantics[@role='button' and contains(text(), 'Back')]")),
-      15000
-    );
-    await backButton.click();
+  const btnSexo = await driver.wait( until.elementLocated(By.xpath("//flt-semantics[@role='button' and contains(text(), 'Sexo')]")), 10000);
+  await btnSexo.click();
 
-    const helpButton = await driver.wait(
-      until.elementLocated(By.xpath("//flt-semantics[@role='button' and contains(text(), 'Ajuda')]")),
-      15000
-    );
-    await helpButton.click();
-    await driver.sleep(5000);
-      await driver.takeScreenshot().then((image, err) => {
-          require('fs').writeFile('../fotos/APOS/help_screen.png', image, 'base64', function (err) {
-            if (err == null){
-                console.log('Gravou Foto da Tela de Ajuda');
-            }else{
-                console.log('Erro ->' + err);
-            }
-          });
-        });
+  const optFeminino = await driver.wait(until.elementLocated(By.xpath("//flt-semantics[@role='button' and contains(text(), 'Feminino')]")),10000);
+  await optFeminino.click();
 
-    const backButton2 = await driver.wait(
-      until.elementLocated(By.xpath("//flt-semantics[@role='button' and contains(text(), 'Back')]")),
-      15000
-    );
-    await backButton2.click();
+  const inputIdade = await driver.wait(until.elementLocated(By.css("input[aria-label='Idade atual']")),10000);
+  await inputIdade.sendKeys("62");
 
+  const inputContribuicao = await driver.wait(until.elementLocated(By.css("input[aria-label='Anos de contribuição']")),10000);
+  await inputContribuicao.sendKeys("35");
+
+  const btnCalcular = await driver.wait(until.elementLocated(By.xpath("//flt-semantics[@role='button' and contains(text(), 'Calcular')]")),10000);
+  await btnCalcular.click();
+
+  await driver.sleep(5000);
+  await driver.takeScreenshot().then((image, err) => {
+      require('fs').writeFile('../fotos/APOS/calculo_aposentadoria_result.png', image, 'base64', function (err) {
+        if (err == null) {
+          console.log('Gravou Foto da Tela de Calculo de Aposentadoria Resultado');
+        } else {
+          console.log('Erro ->' + err);
+        }
+      });
+  });
 
   await driver.sleep(15000);
   driver.quit();
